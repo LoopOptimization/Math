130b382719f2b9248b3aa22422cef6bc196b19c1
Chris Elrod <elrodc@gmail.com>
2024-06-11 00:27:33 -0400

    pretty printer improvements and fixes

--- tools/prettyprinters.py
+++ tools/prettyprinters.py
@@ -10,7 +10,7 @@ class VectorPrinter(Iterator):
         self.val = val
         t = val.type.template_argument(0).pointer()
         self.begin = val["ptr"].cast(t)
-        self.size = val["sz"]["value_"].cast(gdb.lookup_type('long'))
+        self.size = val["sz"]["value_"].cast(gdb.lookup_type("long"))
         self.i = 0
 
     def __next__(self):
@@ -27,7 +27,7 @@ class VectorPrinter(Iterator):
         return "array"
 
 
-class BaseMatrixPrinter(Iterator):
+class BaseMatrixPrinter:
     """Print a StridedMatrix<>"""
 
     def __init__(self, begin, rows, cols, stride):
@@ -35,42 +35,34 @@ class BaseMatrixPrinter(Iterator):
         self.rows = rows
         self.cols = cols
         self.stride = stride
-        self.r = 0
-        self.c = -1
-
-    def __next__(self):
-        if (self.rows == 0) or (self.cols == 0):
-            raise StopIteration
-        self.c += 1
-        if self.c == self.cols:
-            self.c = 0
-            self.r += 1
-            if self.r == self.rows:
-                raise StopIteration
-        ind = "[{}, {}]".format(self.r, self.c)
-        r = (self.begin + (self.c + self.r * self.stride)).dereference()
-        v = str(r)
-        if r >= 0:
-            v = " " + v
-        if (self.c == 0) and (self.r != 0):
-            v = "\n    " + v
-        if (self.r == self.rows - 1) and (self.c == self.cols - 1):
-            v = v + " "
-        return ind, v
 
     def to_string(self):
-        return "Matrix, {} x {}, stride {}:\n".format(self.rows, self.cols, self.stride)
-
-    def display_hint(self):
-        return "array"
-
+        header = "Matrix, {} x {}, stride {}:\n".format(
+            self.rows, self.cols, self.stride
+        )
+        s = [
+            [
+                str((self.begin + (c + r * self.stride)).dereference())
+                for c in range(self.cols)
+            ]
+            for r in range(self.rows)
+        ]
+        # header += len(s[0][0])
+        lens = [max(len(val) for val in col) for col in zip(*s)]
+        # gdb.write("Lens: {}, {}, {}".format(lens[0], lens[1], lens[2]))
+        try:
+            fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+        except Exception as exc:
+            gdb.write("Exception: {}\n".format(exc))
+        table = [fmt.format(*row) for row in s]
+        return header + '\n'.join(table)
 
 class SquareMatrixPrinter(BaseMatrixPrinter):
     """Print a Matrix<>"""
 
     def __init__(self, val):
         t = val.type.template_argument(0).pointer()
-        M = val["sz"]["m_"]["value_"].cast(gdb.lookup_type('long'))
+        M = val["sz"]["m_"]["value_"].cast(gdb.lookup_type("long"))
         BaseMatrixPrinter.__init__(self, val["ptr"].cast(t), M, M, M)
 
 
@@ -79,8 +71,8 @@ class DenseMatrixPrinter(BaseMatrixPrinter):
 
     def __init__(self, val):
         t = val.type.template_argument(0).pointer()
-        M = val["sz"]["m_"]["value_"].cast(gdb.lookup_type('long'))
-        N = val["sz"]["n_"]["value_"].cast(gdb.lookup_type('long'))
+        M = val["sz"]["m_"]["value_"].cast(gdb.lookup_type("long"))
+        N = val["sz"]["n_"]["value_"].cast(gdb.lookup_type("long"))
         BaseMatrixPrinter.__init__(self, val["ptr"].cast(t), M, N, N)
 
 
@@ -92,9 +84,9 @@ class StridedMatrixPrinter(BaseMatrixPrinter):
         BaseMatrixPrinter.__init__(
             self,
             val["ptr"].cast(t),
-            val["sz"]["m_"]["value_"].cast(gdb.lookup_type('long')),
-            val["sz"]["n_"]["value_"].cast(gdb.lookup_type('long')),
-            val["sz"]["stride_m_"]["value_"].cast(gdb.lookup_type('long')),
+            val["sz"]["m_"]["value_"].cast(gdb.lookup_type("long")),
+            val["sz"]["n_"]["value_"].cast(gdb.lookup_type("long")),
+            val["sz"]["stride_m_"]["value_"].cast(gdb.lookup_type("long")),
         )
 
 
@@ -102,17 +94,17 @@ class WrappedIntegerPrinter:
     """Print an integer wrapped in a struct."""
 
     def __init__(self, val):
-        self.val = str(val["value_"])
+        self.val = val["value_"].cast(gdb.lookup_type("long"))
 
     def to_string(self):
-        return self.val
+        return str(self.val)
 
 
 pp = gdb.printing.RegexpCollectionPrettyPrinter("PolyMath")
 pp.add_printer(
     "poly::math::PtrVector",
     "^poly::math::(Mut)?Array<.*, poly::math::axis::Length<-1l, long>,[^,]*?>$",
-    VectorPrinter
+    VectorPrinter,
 )
 pp.add_printer(
     "poly::math::Vector",
@@ -141,19 +133,23 @@ pp.add_printer(
 )
 pp.add_printer(
     "poly::math::DenseMatrix",
-    "^poly::math::ManagedArray<.*, poly::math::DenseDims<-1l>,[^,]*?,[^,]*?>$",
+    "^poly::math::ManagedArray<.*, poly::math::DenseDims<-1l, -1l>,[^,]*?,[^,]*?>$",
     DenseMatrixPrinter,
 )
 pp.add_printer(
     "poly::math::StridedMatrix",
-    "^poly::math::ManagedArray<.*, poly::math::StridedDims<-1l>,[^,]*?,[^,]*?>$",
+    "^poly::math::ManagedArray<.*, poly::math::StridedDims<-1l, -1l, -1l>,[^,]*?,[^,]*?>$",
     StridedMatrixPrinter,
 )
 pp.add_printer(
-    "poly::math::axis::Length", "poly::math::axis::Length<-1l, long>", WrappedIntegerPrinter
+    "poly::math::axis::Length",
+    "poly::math::axis::Length<-1l, long>",
+    WrappedIntegerPrinter,
 )
 pp.add_printer(
-    "poly::math::axis::Capacity", "poly::math::axis::Capacity<-1l, long>", WrappedIntegerPrinter
+    "poly::math::axis::Capacity",
+    "poly::math::axis::Capacity<-1l, long>",
+    WrappedIntegerPrinter,
 )
 pp.add_printer(
     "poly::math::axis::Row", "poly::math::axis::Row<-1l>", WrappedIntegerPrinter
