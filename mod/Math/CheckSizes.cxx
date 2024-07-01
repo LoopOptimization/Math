de2e5258741cdf84f91d823489ceedf512732e5b
Chris Elrod <elrodc@gmail.com>
2024-07-01 10:00:41 -0400

    add CheckSizes file

---
+++ mod/Math/CheckSizes.cxx
@@ -0,0 +1,46 @@
+module;
+#include <concepts>
+#include <cstddef>
+#include <type_traits>
+
+export module CheckSizes;
+
+import Invariant;
+import Param;
+
+template <typename T>
+concept ShouldView = requires(const T &x) {
+  { x.view() } -> utils::TriviallyCopyable;
+};
+
+export namespace math {
+using utils::invariant;
+// inputs must be `ptrdiff_t` or `std::integral_constant<ptrdiff_t,value>`
+template <typename X, typename Y>
+[[gnu::always_inline]] constexpr auto check_sizes(X x, Y y) {
+  if constexpr (std::same_as<ptrdiff_t, X>) {
+    if constexpr (std::same_as<ptrdiff_t, Y>) {
+      invariant(x, y);
+      return x;
+    } else if constexpr (y > 1) {
+      constexpr ptrdiff_t L = y;
+      invariant(x, L);
+      return std::integral_constant<ptrdiff_t, L>{};
+    } else return x;
+  } else if constexpr (x <= 1) return y;
+  else if constexpr (std::same_as<ptrdiff_t, Y>) {
+    constexpr ptrdiff_t L = x;
+    invariant(L, y);
+    return std::integral_constant<ptrdiff_t, L>{};
+  } else if constexpr (y <= 1) return x;
+  else {
+    static_assert(x == y);
+    return std::integral_constant<ptrdiff_t, ptrdiff_t(x)>{};
+  }
+}
+
+template <typename T> [[gnu::always_inline]] constexpr auto view(const T &x) {
+  if constexpr (ShouldView<T>) return x.view();
+  else return x;
+}
+} // namespace math
