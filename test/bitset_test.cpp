e5f9f95d4834ae53da2abc51ba0ff4d007d5b406
Chris Elrod <elrodc@gmail.com>
2025-07-07 13:43:27 -0400

    checkpoint

--- test/bitset_test.cpp
+++ test/bitset_test.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+import boost.ut;
 
 import BitSet;
 import CorePrint;
@@ -9,40 +9,43 @@ import std;
 #define STRINGIZE(x) STRINGIZE_DETAIL(x)
 using containers::BitSet, math::Vector;
 
+int main() {
+  using namespace boost::ut;
+
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-TEST(BitSet, BasicAssertions) {
+"BitSet BasicAssertions"_test = [] {
   BitSet bs(1000);
   std::ptrdiff_t count = 0;
   for (std::ptrdiff_t _ : bs) ++count;
-  ASSERT_EQ(count, 0);
+  expect(fatal(count == 0));
   bs[4] = true;
   bs[10] = true;
   bs[200] = true;
   bs[117] = true;
   bs[87] = true;
   bs[991] = true;
-  ASSERT_EQ(bs.findFirstZero(), 0);
+  expect(fatal(bs.findFirstZero() == 0));
   bs[0] = true;
-  ASSERT_EQ(bs.findFirstZero(), 1);
+  expect(fatal(bs.findFirstZero() == 1));
   bs.print();
   utils::print('\n');
-  // EXPECT_EQ(std::ranges::begin(bs), bs.begin());
-  // EXPECT_EQ(std::ranges::end(bs), bs.end());
+  // expect(std::ranges::begin(bs), bs.begin());
+  // expect(std::ranges::end(bs), bs.end());
   Vector<std::size_t> bsc{std::array{0, 4, 10, 87, 117, 200, 991}};
   std::ptrdiff_t j = 0;
   for (auto J = bs.begin(); J != decltype(bs)::end(); ++J) {
-    EXPECT_EQ(*J, bsc[j++]);
-    EXPECT_TRUE(bs[*J]);
+    expect(*J == bsc[j++]);
+    expect(bs[*J]);
     utils::println("We get: ", *J);
   }
   j = 0;
   for (auto i : bs) {
-    EXPECT_EQ(i, bsc[j++]);
-    EXPECT_TRUE(bs[i]);
+    expect(i == bsc[j++]);
+    expect(bs[i]);
     utils::println("We get: ", i);
   }
-  EXPECT_EQ(j, bsc.size());
-  EXPECT_EQ(j, bs.size());
+  expect(j == bsc.size());
+  expect(j == bs.size());
   utils::println("About to create empty!");
   BitSet empty;
   std::ptrdiff_t c = 0, d = 0;
@@ -52,91 +55,91 @@ TEST(BitSet, BasicAssertions) {
     d += b;
   }
   utils::println("Iterated empty!");
-  EXPECT_FALSE(c);
-  EXPECT_FALSE(d);
+  expect(!c);
+  expect(!d);
   utils::println("we made it to the end!");
-}
+};
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-TEST(BitSet, Insert) {
+"BitSet Insert"_test = [] {
   BitSet<std::array<std::uint64_t, 2>> bs;
   std::ptrdiff_t count = 0;
   for (std::ptrdiff_t _ : bs) ++count;
-  ASSERT_EQ(count, 0);
+  expect(fatal(count == 0));
   bs.insert(1);
   bs.insert(5);
   bs.insert(6);
   bs.insert(8);
-  EXPECT_EQ(bs.data_[0], 354);
-  EXPECT_EQ(bs.data_[1], 0);
+  expect(bs.data_[0] == 354);
+  expect(bs.data_[1] == 0);
   bs.insert(5);
-  EXPECT_EQ(bs.data_[0], 354);
-}
+  expect(bs.data_[0] == 354);
+};
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-TEST(BitSet, DynSize) {
+"BitSet DynSize"_test = [] {
   BitSet bs, bsd{BitSet<>::dense(11)};
   std::ptrdiff_t count = 0;
   for (std::ptrdiff_t _ : bs) ++count;
-  ASSERT_EQ(count, 0);
+  expect(fatal(count == 0));
   for (std::ptrdiff_t _ : bsd) ++count;
-  ASSERT_EQ(count, 11);
-  EXPECT_EQ(bs.data_.size(), 0);
+  expect(fatal(count == 11));
+  expect(bs.data_.size() == 0);
   bs[4] = true;
   bs[10] = true;
-  EXPECT_EQ(bs.data_.size(), 1);
-  EXPECT_EQ(bs.data_.front(), 1040);
+  expect(bs.data_.size() == 1);
+  expect(bs.data_.front() == 1040);
   for (std::ptrdiff_t i = 0; i < 11; ++i)
 #if !defined(__clang__) && defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdangling-else"
 #endif
-    if (!bs.contains(i)) EXPECT_TRUE(bsd.remove(i));
+    if (!bs.contains(i)) expect(bsd.remove(i));
 #if !defined(__clang__) && defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif
-  EXPECT_EQ(bs, bsd);
+  expect(bs == bsd);
   Vector<std::size_t> sv;
   for (auto i : bs) sv.push_back(i);
-  EXPECT_EQ(sv.size(), 2);
-  EXPECT_EQ(sv[0], 4);
-  EXPECT_EQ(sv[1], 10);
-}
+  expect(sv.size() == 2);
+  expect(sv[0] == 4);
+  expect(sv[1] == 10);
+};
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-TEST(BitSet, FixedSize) {
+"BitSet FixedSize"_test = [] {
   BitSet<std::array<std::uint64_t, 2>> bs;
   bs[4] = true;
   bs[10] = true;
-  EXPECT_EQ(bs.data_[0], 1040);
-  EXPECT_EQ(bs.data_[1], 0);
+  expect(bs.data_[0] == 1040);
+  expect(bs.data_[1] == 0);
   Vector<std::size_t> sv;
   for (auto i : bs) sv.push_back(i);
-  EXPECT_EQ(sv.size(), 2);
-  EXPECT_EQ(sv[0], 4);
-  EXPECT_EQ(sv[1], 10);
-}
+  expect(sv.size() == 2);
+  expect(sv[0] == 4);
+  expect(sv[1] == 10);
+};
 // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-TEST(BitSet, FixedSizeSmall) {
+"BitSet FixedSizeSmall"_test = [] {
   using SB = BitSet<std::array<std::uint16_t, 1>>;
   static_assert(sizeof(SB) == 2);
   SB bs;
   std::ptrdiff_t count = 0;
   for (std::ptrdiff_t _ : bs) ++count;
-  ASSERT_EQ(count, 0);
+  expect(fatal(count == 0));
   bs[4] = true;
   bs[10] = true;
   bs[7] = true;
   bs.insert(5);
-  EXPECT_EQ(bs.data_[0], 1200);
+  expect(bs.data_[0] == 1200);
   Vector<std::size_t> sv;
   for (auto i : bs) sv.push_back(i);
-  EXPECT_EQ(sv.size(), 4);
-  EXPECT_EQ(sv[0], 4);
-  EXPECT_EQ(sv[1], 5);
-  EXPECT_EQ(sv[2], 7);
-  EXPECT_EQ(sv[3], 10);
-  EXPECT_EQ(SB::fromMask(1200).data_[0], 1200);
-}
+  expect(sv.size() == 4);
+  expect(sv[0] == 4);
+  expect(sv[1] == 5);
+  expect(sv[2] == 7);
+  expect(sv[3] == 10);
+  expect(SB::fromMask(1200).data_[0] == 1200);
+};
 // NOLINTNEXTLINE()
-TEST(BitSet, IterTest) {
+"BitSet IterTest"_test = [] {
   math::Vector<std::uint64_t, 1> data{};
   data.push_back(9223372036854775808U);
   data.push_back(1732766553700568065U);
@@ -148,5 +151,8 @@ TEST(BitSet, IterTest) {
     ++i;
   }
   utils::print('\n');
-  ASSERT_EQ(i, sz);
+  expect(fatal(i == sz));
+};
+
+  return 0;
 }
